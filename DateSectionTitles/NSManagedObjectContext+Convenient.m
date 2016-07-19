94dc3260958a564938d88724ef9c89f167fb63a1
Marcelo Gobetti <marcelo.gobetti@arctouch.com>
2016-07-19 17:39:36 -0300

    Ensures that all inserted objects have permanent objectID's.

--- DateSectionTitles/NSManagedObjectContext+Convenient.m
+++ DateSectionTitles/NSManagedObjectContext+Convenient.m
@@ -28,7 +28,12 @@
 // copied from Apple's APLAppDelegate
 - (void)saveAndHandle {
     NSError *error;
-    if ([self hasChanges] && ![self save:&error]) {
+    if (![self hasChanges]) {
+        return;
+    }
+    
+    if (![self obtainPermanentIDsForObjects:self.insertedObjects.allObjects error:&error]
+        || ![self save:&error]) {
         // Replace this implementation with code to handle the error appropriately.
         // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
         NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
