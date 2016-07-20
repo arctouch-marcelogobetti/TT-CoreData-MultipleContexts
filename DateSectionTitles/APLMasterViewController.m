aa234b4d7985606e690932d12b6f85700cbee401
Marcelo Gobetti <marcelo.gobetti@arctouch.com>
2016-07-20 10:40:32 -0300

    Replaced rollback by undo to revert only the last unsaved change.
    Why it does not work..? Next commit!

--- DateSectionTitles/APLMasterViewController.m
+++ DateSectionTitles/APLMasterViewController.m
@@ -199,7 +199,7 @@
                 // undo changes:
                 [[self uiMoc] performBlock:^{
                     NSError* error;
-                    [[self uiMoc] rollback]; // reverts ALL unsaved changes in the uiMoc
+                    [[self uiMoc] undo]; // reverts the last unsaved change in the uiMoc
                     [[self uiMoc] save:&error]; // no need to save parent contexts
                     NSAssert(error == nil, @"Error while trying to save the UI MOC after reverting deletion of object: %@", [error userInfo]);
                 }];
